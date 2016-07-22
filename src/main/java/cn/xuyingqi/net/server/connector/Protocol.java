ed0c7247e49d8eb33b9ba38ae79b5f2da1c0ff44
XuYQ <XuYQ@XuYQ-PC>
2016-07-22 20:21:36 +0800

    x

---
+++ src/main/java/cn/xuyingqi/net/server/connector/Protocol.java
@@ -0,0 +1,11 @@
+package cn.xuyingqi.net.server.connector;
+
+/**
+ * 协议
+ * 
+ * @author XuYQ
+ *
+ */
+public interface Protocol {
+
+}
---
+++ src/main/java/cn/xuyingqi/net/server/container/ProtocolContainer.java
@@ -0,0 +1,20 @@
+package cn.xuyingqi.net.server.container;
+
+import cn.xuyingqi.net.server.connector.Protocol;
+
+/**
+ * 协议容器
+ * 
+ * @author XuYQ
+ *
+ */
+public interface ProtocolContainer {
+
+	/**
+	 * 获取协议
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public Protocol getProtocol(String name);
+}
