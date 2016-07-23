b8a3e8218ba813f00ecbd44a00c5472df8e87c87
oxuyingqio <oxuyingqio@qq.com>
2016-07-23 09:10:01 +0800

    X

---
+++ src/main/java/cn/xuyingqi/net/server/connector/datagram/Datagram.java
@@ -0,0 +1,24 @@
+package cn.xuyingqi.net.server.connector.datagram;
+
+/**
+ * 数据报文
+ * 
+ * @author XuYQ
+ *
+ */
+public interface Datagram {
+
+	/**
+	 * 获取报头
+	 * 
+	 * @return
+	 */
+	public Header getHeader();
+
+	/**
+	 * 获取报体
+	 * 
+	 * @return
+	 */
+	public byte[] getPayload();
+}
---
+++ src/main/java/cn/xuyingqi/net/server/connector/datagram/Header.java
@@ -0,0 +1,11 @@
+package cn.xuyingqi.net.server.connector.datagram;
+
+/**
+ * 报头
+ * 
+ * @author XuYQ
+ *
+ */
+public interface Header {
+
+}
